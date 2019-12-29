c019bbdea234424e66a095bc7bf0855da327396a
hbsnow <yuki.4uing@gmail.com>
2019-12-29 13:01:38 +0900

    refact

--- src/app/pages/signin/signin.component.ts
+++ src/app/pages/signin/signin.component.ts
@@ -1,7 +1,6 @@
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from 'src/app/services/authentication.service';
-import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
